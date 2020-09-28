1a5f00d3b79a198ebd0c00aadd1fe2c96e74d4ed
lokesh234 <lgangaramaney@wpi.edu>
2020-09-28 17:58:13 -0400

    Completed Project Partners project

---
+++ src/components/IQPTable.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import TableData from "./IQPTableData";
+import MaterialTable from "material-table";
+import Box from "@material-ui/core/Box";
+
+const colnames = [
+    {
+        title: 'Sponsor', field: 'Sponsor'
+    },
+    {
+        title: 'Location', field: 'Location'
+    }
+]
+
+const IQPTable =  ({ tabledata }) => {
+        return(
+            <div>
+                <Box mt={2} />
+                <MaterialTable title="IQPs"
+                    columns={colnames.map((c) => ({ ...c, tableData: undefined }))}
+                               data={tabledata}
+                               options={{
+                                   search: true,
+                                   exportButton: true,
+                                   pageSize:5,
+                                   pageSizeOptions: [10]
+                               }}/>
+            </div>
+        )
+}
+
+export default IQPTable;
+
